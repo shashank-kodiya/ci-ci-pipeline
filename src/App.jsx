d4f51d3cce316b2b0c739ccb4ddc21786f1c2dcd
shashank-kodiya <shashankkodiyashashank@gmail.com>
2026-07-30 14:24:19 +0530

    Update welcome message in App component

--- src/App.jsx
+++ src/App.jsx
@@ -20,7 +20,7 @@ function App() {
           letterSpacing: "1.2px"
         }}
       >
-        Welcome To Ethnotech  and my website !!!!!!!!!!!!!!!!!!!!!!!!!!
+        Welcome To Ethnotech  and my website  how are you!!!!!!!!!!!!!!!!!!!!!!!!!!
       </h1> 
     </div>
   );
